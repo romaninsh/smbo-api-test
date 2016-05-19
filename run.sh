ba7cd5cd5479acdd6100f77cfc6aa1ca112784ba
Romans Malinovskis <romans@agiletoolkit.org>
2016-05-19 11:48:12 +0100

    fix

---
+++ run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# See if volume needs initializing
+/volume-init.sh
+/live.sh
+
+read pid cmd state ppid pgrp session tty_nr tpgid rest < /proc/self/stat
+trap "kill -TERM -$pgrp; exit" EXIT TERM KILL SIGKILL SIGTERM SIGQUIT
+
+source /etc/apache2/envvars
+exec apache2 -D FOREGROUND
