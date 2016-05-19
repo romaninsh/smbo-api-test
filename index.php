1b2dbf9fc8bac56383673b59d1d04fcaea0fbc99
Romans Malinovskis <romans@agiletoolkit.org>
2016-05-19 11:49:13 +0100

    fix

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+
+// This project is written in Agile Toolkit.
+// See http://agiletoolkit.org/
+
+
+include 'vendor/autoload.php';
+include 'lib/Frontend.php';
+$api=new Frontend('sample_project');
+$api->main();
+?>
